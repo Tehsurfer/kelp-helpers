1fd1211c6e79e7992c38ea2cec628da1f327ddea
Jesse Khorasanee <jessekhorasanee@gmail.com>
2025-06-11 21:42:21 +0800

    Switch from index.js to main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
 import './index.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import './reportWebVitals.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
